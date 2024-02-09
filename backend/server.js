95ad0e1f1d43a900688ae9c3181dedaf0d18cb79
Azizul Hakim Tareq <azizul.tareq@northsouth.edu>
2024-02-09 15:48:13 +0600

    mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDatabase from "./config/db.js";
 
 dotenv.config();
+
+connectDatabase();
 
 const app = express();
 app.use(cors());
@@ -12,7 +15,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-  )
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
